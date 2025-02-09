1bbc328f0592201aee26ed18da977fbc08093e07
captainurist <73941350+captainurist@users.noreply.github.com>
2025-02-09 16:34:25 +0000

    Tile generator WIP

---
+++ src/Engine/Graphics/TileGenerator.cpp
@@ -0,0 +1,2 @@
+#include "TileGenerator.h"
+
--- src/Utility/Flags.h
+++ src/Utility/Flags.h
@@ -155,3 +155,10 @@ typename Flags<Enum>::underlying_type to_underlying(Flags<Enum> flags) {
     return static_cast<typename Flags<Enum>::underlying_type>(flags);
 }
 } // namespace std
+
+template<class Enum>
+struct std::hash<Flags<Enum>> {
+    size_t operator()(Flags<Enum> value) const {
+        return std::hash<typename Flags<Enum>::underlying_type>()(std::to_underlying(value));
+    }
+};
